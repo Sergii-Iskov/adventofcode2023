1d2b41744e305efa8415214795399ea75cffad3a
Sergii-Iskov <113548734+Sergii-Iskov@users.noreply.github.com>
2023-12-02 17:33:50 +0200

    Add files via upload

---
+++ testDay1.java
@@ -0,0 +1,9 @@
+package com.shpp.p2p.cs.siskov.adventofcode;
+
+public class testDay1 {
+    public static void main(String[] args) throws Exception {
+        day1 obj = new day1();
+//        day1.main(new String[]{"testDay1-2.txt"});
+        day1.main(new String[]{"testDay1-1.txt"});
+    }
+}
